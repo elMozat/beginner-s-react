f0f89d005842608591aa041338a08cf5a7d984f1
elMozat <mosesaubyn1984@gmail.com>
2022-01-22 03:41:12 -0500

    first changes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,17 @@ import React, { useState } from 'react';
 import Tweet from './tweet';
 
 function App() {
-  const [isRed, setRed] = useState(false);
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-    setRed(true);
-    setRed(!isRed);
-  };
+  const [users, setUsers] = useState([
+    { name: 'Ed', message: 'Hello there' },
+    { name: 'John', message: 'I am John Snaow' },
+    { name: 'Elton', message: 'I am awesome' },
+  ]);
 
   return (
     <div className="app">
-      <h1 className={isRed ? 'red' : ''}>Change my color</h1>
-      <button onClick={increment}>Increment</button>
-      <h1>{count}</h1>
+      {users.map((user) => (
+        <Tweet name={user.name} message={user.message} />
+      ))}
     </div>
   );
 }
